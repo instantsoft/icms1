7e4c9313158b76c60dfed71e679f080cc174cb0c
maxicms <support@maxicms.ru>
2015-12-14 22:20:46 +0200

    Добавлена поддержка создания райнов и метро для компонента Геолокация.

--- migrate/index.php
+++ migrate/index.php
@@ -188,6 +188,23 @@ $version_next = '1.10.6';
 
         echo '<p>Поддержка мультиязычности компонентов контент и форум выполнена</p>';
 
+        $inDB->query("CREATE TABLE IF NOT EXISTS `#__geo_districts` (
+                          `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+                          `country_id` int(11) unsigned NOT NULL DEFAULT '0',
+                          `region_id` int(10) unsigned NOT NULL DEFAULT '0',
+                          `city_id` int(10) unsigned NOT NULL DEFAULT '0',
+                          `name` varchar(128) NOT NULL DEFAULT '',
+                          `published` int(1) NOT NULL DEFAULT '1',
+                          `ordering` int(11) NOT NULL DEFAULT '1',
+                          PRIMARY KEY (`id`),
+                          KEY `country_id` (`country_id`),
+                          KEY `region_id` (`region_id`),
+                          KEY `city_id` (`city_id`),
+                          KEY `name` (`name`)
+                      ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;");
+
+        echo '<p>Поддержка районов и метро в компоненте Геолокация выполнена</p>';
+
 		echo '<div style="margin:15px 0px;font-weight:bold">Миграция завершена. Удалите папку /migrate/ прежде чем продолжить!</div>';
 		echo '<div class="nextlink"><a href="/">Перейти на сайт</a></div>';
 
