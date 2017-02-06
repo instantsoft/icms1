89848ff51338e71618a678777517ffcf88a4730c
fuze <fuze@localhost>
2017-02-06 10:43:27 +0300

    PHPMailer fix

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 //                           InstantCMS v1.10.7                               //
 //                        http://www.instantcms.ru/                           //
 //                                                                            //
-//                   written by InstantCMS Team, 2007-2016                    //
+//                   written by InstantCMS Team, 2007-2017                    //
 //                produced by InstantSoft, (www.instantsoft.ru)               //
 //                                                                            //
 //                        LICENSED BY GNU/GPL v2                              //
